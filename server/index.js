de09129566ce392e97049c19b4fa0e4a4d55219f
ShobhitNair <shobhit842160@gmail.com>
2024-03-14 13:36:49 +0530

    mongo

--- server/index.js
+++ server/index.js
@@ -45,6 +45,8 @@ app.get('/',(req,res)=>{
 })
 
 mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 }).then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 }).catch((error) => console.log(`${error} did not connect`));
